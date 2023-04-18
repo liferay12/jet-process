e0ba34b81eeb54ed29e85e67634d7ea028ad63f3
Lohita Sagar Mishra <lohitamishra77@gmail.com>
2023-04-18 10:29:45 +0530

    update

--- src/component/File.jsx
+++ src/component/File.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import fileFormJSOn from "../json-data/fileForm.json";
 import Form from '../library/renderer/FormRenderer';
 import { DataTabel } from './DataTabel';
-export const File = (props) => {
+export const File = () => {
     let url = "";
     return (
         <div className='Home'>
             <div className='row'>
                 <div className='col-md-6 offset-md-3 card'>
-                    <Form formObject={fileFormJSOn} editData={props.data} url={url} />
+                    <Form formObject={fileFormJSOn} editData={{}} url={url} />
                 </div>
                 {/* <DataTabel data={FilterUsers} setSearch={setSearch} url={""}></DataTabel> */}
             </div>
